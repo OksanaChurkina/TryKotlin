cf7a600e5268d1328b0bb552721a1ffd424941f5
oks <churkinaksana@gmail.com>
2020-06-06 20:09:54 +0300

    runblocking

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,29 @@
 import kotlinx.coroutines.*
+import java.text.SimpleDateFormat
+import java.util.*
 
-fun main(args: Array<String>) {
-    println("Start")
 
-    // Start a coroutine
-    GlobalScope.launch {
-        delay(1000)
-        println("Hello")
-    }
+fun main() = runBlocking {
+    val deferred1 = async { computation1() }
+    val deferred2 = async { computation2() }
+    printCurrentTime("Awaiting computations...")
+    val result = deferred1.await() + deferred2.await()
+    printCurrentTime("The result is $result")
+}
 
-    Thread.sleep(2000) // wait for 2 seconds
-    println("Stop")
+suspend fun computation1(): Int {
+    delay(1000L) // simulated computation
+    printCurrentTime("Computation1 finished")
+    return 131
+}
 
+suspend fun computation2(): Int {
+    delay(2000L)
+    printCurrentTime("Computation2 finished")
+    return 9
+}
+
+fun printCurrentTime(message: String) {
+    val time = (SimpleDateFormat("hh:mm:ss")).format(Date())
+    println("[$time] $message")
 }

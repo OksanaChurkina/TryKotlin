df4ca86ad1d8c6cfbd4624564cd150cb25d29227
oks <churkinaksana@gmail.com>
2020-06-06 19:14:19 +0300

    coroutines

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+import kotlinx.coroutines.*
+
+fun main(args: Array<String>) {
+    println("Start")
+
+    // Start a coroutine
+    GlobalScope.launch {
+        delay(1000)
+        println("Hello")
+    }
+
+    Thread.sleep(2000) // wait for 2 seconds
+    println("Stop")
+
+}
